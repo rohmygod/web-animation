2424b9c50e54026b91205000ba2c2c9a3d69ff56
Romy <romylenten@gmail.com>
2020-05-24 19:12:25 +0200

    Vuurwerk, js, fiets update

---
+++ java.js
@@ -0,0 +1,18 @@
+
+// document.addEventListener('keydown', (shift) => {
+//      if(shift.key == “32”)
+//      {
+// 	spatie.classList.toggle(“.explosion”);
+//      }
+
+// });
+
+var fire = document.getElementById('vuurwerk');
+	window.addEventListener('keydown', toggle);
+
+	function toggle(event){
+		if (event.keyCode === 38){
+			fire.classList.toggle('show');
+			fire.classList.remove('hidden');
+		}
+	}
